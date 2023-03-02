afa41ec3b0d51d71364b074d37195c48b1c271d7
phbui <101759075+phbui@users.noreply.github.com>
2023-03-02 18:50:36 -0500

    Minor changes
    Added:
    - music
    
    Changed:
    - start menu functionality
    - certain icons

--- windowsxp/js/start-menu.js
+++ windowsxp/js/start-menu.js
@@ -1,5 +1,48 @@
 $(document).ready(function(){
-  $(".start-btn").click(function(){
-    $('.start-menu-win').toggleClass('active-menu');
-  })
-})
+  var startMenu = $('.start-menu-win');
+  var startButton = $('.start-btn');
+  var isDragging = false;
+  var initialX;
+  var initialY;
+  var xOffset = 0;
+  var yOffset = 0;
+  
+  startMenu.mousedown(function(event) {
+    isDragging = true;
+    initialX = event.clientX - xOffset;
+    initialY = event.clientY - yOffset;
+  });
+  
+  $(document).mousemove(function(event) {
+    if(isDragging) {
+      var currentX = event.clientX - initialX;
+      var currentY = event.clientY - initialY;
+      
+      xOffset = currentX;
+      yOffset = currentY;
+      
+      startMenu.css({
+        'left': currentX + 'px',
+        'top': currentY + 'px'
+      });
+    }
+  });
+  
+  $(document).mouseup(function() {
+    isDragging = false;
+    startMenu.removeClass('active-menu');
+    startButton.removeClass('selected');
+  });
+  
+  startButton.click(function(event) {
+    startMenu.toggleClass('active-menu');
+    event.stopPropagation();
+  });
+  
+  $(document).click(function(event) { 
+    if(startMenu.hasClass('active-menu') && !$(event.target).closest(startMenu).length && event.target !== startButton[0]) {
+      startMenu.removeClass('active-menu');
+      startButton.removeClass('selected');
+    }        
+  });
+});
