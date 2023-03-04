cc20de6520a7a6525bbf72c78e5a8fae44b5b46e
phbui <101759075+phbui@users.noreply.github.com>
2023-03-04 15:06:58 -0500

    wallpaper speend and spacing changes for archive

--- windowsxp/js/wallpaper.js
+++ windowsxp/js/wallpaper.js
@@ -10,8 +10,27 @@ const wallpapers = [
     // add more images as needed
   ];
 
-let currentIndex = 1;
-    setInterval(() => {
-    document.body.style.backgroundImage = wallpapers[currentIndex];
+  const rangeInput = document.getElementById("range26");
+
+  let currentIndex = 1;
+  let interval = rangeInput.value * 250; // convert seconds to milliseconds
+  document.body.style.backgroundImage = wallpapers[currentIndex];
+  
+  rangeInput.addEventListener("input", () => {
+    interval = rangeInput.value * 250;
+    console.log(interval);
+  
+    // clear the existing interval and create a new one with the updated interval value
+    clearInterval(intervalID);
+    intervalID = setInterval(() => {
+      currentIndex = (currentIndex + 1) % wallpapers.length;
+      document.body.style.backgroundImage = wallpapers[currentIndex];
+    }, interval);
+  });
+  
+  // set the initial interval
+  let intervalID = setInterval(() => {
     currentIndex = (currentIndex + 1) % wallpapers.length;
-}, 250); // change background every 5 seconds (adjust as needed)
+    document.body.style.backgroundImage = wallpapers[currentIndex];
+  }, interval);
+  
