c98e6ab94e0845366bcf3f3e79b0bd45dc6fd6bb
phbui <101759075+phbui@users.noreply.github.com>
2023-03-04 17:07:34 -0500

    minor changes
    - added more values to wallpaper speed slider
    - bounded dragging

--- windowsxp/js/window.js
+++ windowsxp/js/window.js
@@ -1,50 +1,45 @@
-$(document).ready(function() {
-  // Make all windows draggable
-  $(".window").draggable({
-    start: function(event, ui) {
-      // Get the highest z-index value of all windows
-      var maxZIndex = 0;
-      $(".window").each(function() {
-        var zIndex = parseInt($(this).css("z-index"));
-        if (zIndex > maxZIndex) {
-          maxZIndex = zIndex;
-        }
-      });
+$(function() {
+  let windows = $(".window");
+  let maxZIndex = 0;
 
+  windows.draggable({
+    start: function(event, ui) {
       // Set the dragged window's z-index to the highest value
-      $(this).css("z-index", maxZIndex + 1);
+      $(this).css("z-index", ++maxZIndex);
+
+      // Check if the mouse is within the bounds of the body
+      if (event.pageX < $('body').offset().left || 
+          event.pageY < $('body').offset().top || 
+          event.pageX > $('body').offset().left + $('body').width() || 
+          event.pageY > $('body').offset().top + $('body').height()) {
+        return false;
+      }
 
       // Calculate the offset between the cursor and the window's top-left corner
-      var offset = $(this).offset();
-      var x = event.pageX - offset.left;
-      var y = event.pageY - offset.top;
-      ui.offset = { left: x, top: y };
+      let offset = $(this).offset();
+      ui.offset = { left: event.pageX - offset.left, top: event.pageY - offset.top };
     },
     drag: function(event, ui) {
+      // Check if the mouse is within the bounds of the body
+      if (event.pageX < $('body').offset().left || 
+          event.pageY < $('body').offset().top || 
+          event.pageX > $('body').offset().left + $('body').width() || 
+          event.pageY > $('body').offset().top + $('body').height()) {
+        return false;
+      }
+
       // Update the window's position based on the cursor position and the ui.offset
-      var offset = $(this).offsetParent().offset();
-      var x = event.pageX - offset.left - ui.offset.left;
-      var y = event.pageY - offset.top - ui.offset.top;
+      let parentOffset = $(this).offsetParent().offset();
       $(this).css({
         "position": "absolute",
-        "left": x + "px",
-        "top": y + "px"
+        "left": event.pageX - parentOffset.left - ui.offset.left + "px",
+        "top": event.pageY - parentOffset.top - ui.offset.top + "px"
       });
     }
   });
 
-  // When a window is clicked, bring it to the front
-  $(".window").click(function() {
-    // Get the highest z-index value of all windows
-    var maxZIndex = 0;
-    $(".window").each(function() {
-      var zIndex = parseInt($(this).css("z-index"));
-      if (zIndex > maxZIndex) {
-        maxZIndex = zIndex;
-      }
-    });
-
+  windows.click(function() {
     // Set the clicked window's z-index to the highest value
-    $(this).css("z-index", maxZIndex + 1);
+    $(this).css("z-index", ++maxZIndex);
   });
 });
