bba76509d83ae96f4136a352a8de3b9a5631ac70
phbui <101759075+phbui@users.noreply.github.com>
2023-03-04 17:30:50 -0500

    fixed start menu bug

--- windowsxp/js/start-menu.js
+++ windowsxp/js/start-menu.js
@@ -6,49 +6,53 @@ $(document).ready(function(){
   var initialY;
   var xOffset = 0;
   var yOffset = 0;
-  
+
   startMenu.mousedown(function(event) {
     isDragging = true;
     initialX = event.clientX - xOffset;
     initialY = event.clientY - yOffset;
   });
-  
+
   $(document).mousemove(function(event) {
     if(isDragging) {
       var currentX = event.clientX - initialX;
       var currentY = event.clientY - initialY;
-      
+
       xOffset = currentX;
       yOffset = currentY;
-      
+
       startMenu.css({
         'left': currentX + 'px',
         'top': currentY + 'px'
       });
     }
   });
-  
+
   $(document).mouseup(function() {
     isDragging = false;
-    startMenu.removeClass('active-menu');
-    startButton.removeClass('selected');
   });
-  
+
   startButton.click(function(event) {
     if (startMenu.hasClass('active-menu')) {
       startMenu.removeClass('active-menu');
-      startButton.removeClass('selected');
     } else {
       startMenu.addClass('active-menu');
-      startButton.addClass('selected');
     }
     event.stopPropagation();
   });
-  
-  $(document).click(function(event) { 
-    if(startMenu.hasClass('active-menu') && !$(event.target).closest(startMenu).length && event.target !== startButton[0]) {
+
+  startMenu.click(function(event) {
+    event.stopPropagation();
+  });
+
+  $(document).click(function(event) {
+    var isStartMenu = $(event.target).closest('.start-menu-win').is(startMenu);
+    var isStartButton = $(event.target).is(startButton);
+
+
+    
+    if (!isStartMenu && !isStartButton) {
       startMenu.removeClass('active-menu');
-      startButton.removeClass('selected');
-    }        
+    }
   });
 });
